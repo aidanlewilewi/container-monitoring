65f93790c50fbb30c4fb386389f6fa6c59d8ec07
Aidan Lewis <alewis8@zagmail.gonzaga.edu>
2022-06-30 17:16:08 -0800

    print function added

--- prometheus.py
+++ prometheus.py
@@ -2,21 +2,21 @@ from prometheus_client import start_http_server, Counter
 import docker
 import json
 
-if __name__ == '__main__':
+stop_events = ['die', 'kill', 'stop', 'exec_die', 'oom']
 
-    stop_events = ['die', 'kill', 'stop', 'exec_die', 'oom']
-
-    start_http_server(8000)
-
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+def print_container_stops(client):
     events = client.events()
-    c = Counter('container_stops', 'Container Stops')
-
     for event in events:
         json_event = json.loads(event)
         try:
             if json_event['status'] in stop_events:
                 print(json_event['status']) 
-                c.inc()
         except:
             pass 
+
+if __name__ == '__main__':
+
+    start_http_server(8000)
+
+    print_container_stops(docker.DockerClient(base_url='unix://var/run/docker.sock'))
+    
